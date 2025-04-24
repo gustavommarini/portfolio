cc91e4e7208380410bb3abad2c1e7fbc550d6865
gustavommarini <gustavommarini@hotmail.com>
2025-04-24 13:09:48 +0200

    [feat]: Add the useTypewriter hook, to mimic typing a text or a text array

---
+++ src/hooks/useTypewriter.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+export const useTypewriter = (
+  text: string | string[],
+  delay: number = 110,
+  infinite: boolean = true
+) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const stringArray =
+    Object.prototype.toString.call(text) === '[object Array]' ? text : [text];
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    if (
+      currentTextIndex < stringArray.length &&
+      currentIndex <= stringArray[currentTextIndex].length
+    ) {
+      timeout = setTimeout(() => {
+        setCurrentText(
+          (prevText) => prevText + stringArray[currentTextIndex][currentIndex]
+        );
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, delay);
+    } else if (infinite) {
+      // ADD THIS CHECK
+      setCurrentIndex(0);
+      setCurrentText(' ');
+      setCurrentTextIndex((prevIndex) =>
+        prevIndex + 1 < stringArray.length ? prevIndex + 1 : 0
+      );
+      //   setCurrentTextIndex(0);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, infinite, text]);
+
+  return currentText;
+};
