e683974a480c6c344bb2a42a6b9ab2dad6e365de
gustavommarini <gustavommarini@hotmail.com>
2025-04-24 13:08:19 +0200

    [fix]: Update i18n for use the LanguageMenu

--- src/language/i18n.ts
+++ src/language/i18n.ts
@@ -3,18 +3,21 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpBackend from 'i18next-http-backend';
 import enHome from './locales/en/home.json';
+import enIntroData from './locales/en/intro_data.json';
 import enSinglePage from './locales/en/single_page.json';
 import esHome from './locales/es/home.json';
 import esSinglePage from './locales/es/single_page.json';
+import esIntroData from './locales/es/intro_data.json';
+import { AvailableLanguages } from './i18n.types';
 
 i18n
   .use(HttpBackend) // Load translations from JSON files
   .use(LanguageDetector) // Detect user language
   .use(initReactI18next) // Pass i18n instance to react-i18next
   .init({
-    lng: 'en', // Set the initial language of the App
-    fallbackLng: 'en', // Default language
-    supportedLngs: ['en', 'es'], // Supported languages
+    lng: AvailableLanguages.en, // Set the initial language of the App
+    fallbackLng: AvailableLanguages.en, // Default language
+    supportedLngs: [AvailableLanguages.en, AvailableLanguages.es], // Supported languages
     debug: true,
     interpolation: {
       escapeValue: false, // React already protects from XSS
@@ -29,10 +32,12 @@ i18n
       en: {
         home: enHome,
         single: enSinglePage,
+        intro: enIntroData,
       },
       es: {
         home: esHome,
         single: esSinglePage,
+        intro: esIntroData,
       },
     },
   });
---
+++ src/language/i18n.types.ts
@@ -0,0 +1,4 @@
+export enum AvailableLanguages {
+  es = 'es',
+  en = 'en',
+}
