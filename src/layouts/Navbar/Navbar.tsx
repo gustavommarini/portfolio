81d48283bcf012ac831b21477ef520c9ac49da82
gustavommarini <gustavommarini@hotmail.com>
2025-04-24 16:43:24 +0200

    [fix]: Update Navbar and LanguageMenu fixed position and removed commented code

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,36 +1,73 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import './navbar.scss';
 import { useTranslation } from 'react-i18next';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Navbar: FC = () => {
   const { t: navTranslation } = useTranslation(['home']);
+  const checkboxRef: React.LegacyRef<HTMLInputElement> | null = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    //Every time the page change I need to close the menu
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false;
+    }
+    //Scroll to top on navigation
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
-      <input type="checkbox" id="active-navmenu" />
+      <input type="checkbox" id="active-navmenu" ref={checkboxRef} />
       <label className="menu-btn" htmlFor="active-navmenu">
         <i className="fas fa-bars"></i>
       </label>
       <div className="wrapper-navmenu">
         <ul id="nav">
           <li className="nav-link">
-            <a href="#" data-name={navTranslation('navbar-items.home')}>
+            <NavLink
+              to="/"
+              className={(isActive) =>
+                'nav-element' + (isActive ? ' selected' : '')
+              }
+              data-name={navTranslation('navbar-items.home')}
+            >
               {navTranslation('navbar-items.home')}
-            </a>
+            </NavLink>
           </li>
           <li className="nav-link">
-            <a href="#" data-name={navTranslation('navbar-items.about')}>
+            <NavLink
+              to="/about"
+              className={(isActive) =>
+                'nav-element' + (isActive ? ' selected' : '')
+              }
+              data-name={navTranslation('navbar-items.about')}
+            >
               {navTranslation('navbar-items.about')}
-            </a>
+            </NavLink>
           </li>
           <li className="nav-link">
-            <a href="#" data-name={navTranslation('navbar-items.experience')}>
+            <NavLink
+              to="/work-experience"
+              className={(isActive) =>
+                'nav-element' + (isActive ? ' selected' : '')
+              }
+              data-name={navTranslation('navbar-items.experience')}
+            >
               {navTranslation('navbar-items.experience')}
-            </a>
+            </NavLink>
           </li>
           <li className="nav-link">
-            <a href="#" data-name={navTranslation('navbar-items.contact')}>
+            <NavLink
+              to="/contact"
+              className={(isActive) =>
+                'nav-element' + (isActive ? ' selected' : '')
+              }
+              data-name={navTranslation('navbar-items.contact')}
+            >
               {navTranslation('navbar-items.contact')}
-            </a>
+            </NavLink>
           </li>
         </ul>
       </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import { useTypewriter } from '@/hooks/useTypewriter';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import './home.scss';
 import { Button } from '@/components';
 import { ButtonType } from '@/components/Button/Button.types';
@@ -37,72 +36,6 @@ const Home = () => {
       </div>
     </section>
   );
-
-  // return (
-  //   <section className="example-section">
-  //     <div className="example-page">
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //       <a href="https://reactrouter.com/" target="_blank">
-  //         <img
-  //           src={reactRouterLogo}
-  //           className="logo react router"
-  //           alt="React router logo"
-  //         />
-  //       </a>
-  //       <a href="https://sass-lang.com/" target="_blank">
-  //         <img src={sassLogo} className="logo sass" alt="Sass logo" />
-  //       </a>
-  //       <a href="https://www.i18next.com/" target="_blank">
-  //         <img src={i18nLogo} className="logo i18next" alt="i18next logo" />
-  //       </a>
-  //       <a href="https://eslint.org/" target="_blank">
-  //         <img src={eslintLogo} className="logo eslint" alt="eslint logo" />
-  //       </a>
-  //       <a href="https://prettier.io/" target="_blank">
-  //         <img
-  //           src={prettierLogo}
-  //           className="logo prettier"
-  //           alt="prettier logo"
-  //         />
-  //       </a>
-  //       <a href="https://typicode.github.io/husky/" target="_blank">
-  //         <div className="text-logo">🐶</div>
-  //       </a>
-  //       <a href="http://jestjs.io/" target="_blank">
-  //         <img src={jestLogo} className="logo jest" alt="jest logo" />
-  //       </a>
-  //       <a href="https://testing-library.com/" target="_blank">
-  //         <img src={rtlLogo} className="logo rtl" alt="rtl logo" />
-  //       </a>
-  //     </div>
-  //     <h1>
-  //       Vite + React + React Router + Sass + i18next + ESLint + Prettier + Husky
-  //       + Jest + RTL
-  //     </h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">{appTranslation('welcome')}</p>
-  //     <p>
-  //       <code>
-  //         {appTranslation('current_language', { ns: 'single' })}:{' '}
-  //         {i18n.language}
-  //       </code>
-  //     </p>
-  //     <button onClick={() => i18n.changeLanguage('en')}>English</button>
-  //     <button onClick={() => i18n.changeLanguage('es')}>Español</button>
-  //   </section>
-  // );
 };
 
 export default Home;
