24d1a80e9e3a7bc40281cb42d9d225c7c003b334
gustavommarini <gustavommarini@hotmail.com>
2025-05-09 17:11:33 +0200

    [feat]: Remove all the console warning message. There were all regarding SASS

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "./src/styles/variables.scss" as *;`,
+        api: 'modern-compiler', // or "modern"
+        additionalData: `@use "@/styles/variables.scss" as *;`,
       },
     },
   },
